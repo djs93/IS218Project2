90b2beedd1a559fda8186343804e1b7b54711483
Dale <djs93@njit.edu>
2020-11-28 01:32:18 -0500

    Work on display questions page

---
+++ views/display_questions.php
@@ -0,0 +1,18 @@
+<?php include('abstract-views/header.php');?>
+
+<a href=".?action=display_question_form&userId=<?php echo $userId;?>">Add Question</a>
+
+<table>
+    <tr>
+        <th>Head</th>
+        <th>Body</th>
+    </tr>
+    <?php foreach ($questions as $question) : ?>
+        <tr>
+            <td><?php echo $question['title']; ?></td>
+            <td><?php echo $question['body']; ?></td>
+        </tr>
+    <?php endforeach; ?>
+</table>
+
+<?php include('abstract-views/footer.php');?>
